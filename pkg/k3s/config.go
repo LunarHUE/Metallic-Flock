25df4490cdccbf9400753b64a13dcb1d9360d1e7
Wesley Baldwin <74224353+Legonois@users.noreply.github.com>
2025-12-11 19:32:40 +0000

    renamed some files, but it all works!

--- pkg/k3s/config.go
+++
@@ -1,8 +0,0 @@
-package k3s
-
-type K3sConfig struct {
-	Token  string `yaml:"token,omitempty"`
-	Server string `yaml:"server,omitempty"` // URL of the controller to join
-	// Add other flags as needed
-	TLSSan []string `yaml:"tls-san,omitempty"`
-}
